1011a3991aaca3ce9aa6b9ad5bb620f087560091
Bianca Pascu <biancapascu19@yahoo.com>
2022-10-11 19:22:48 +0300

    Controller cu cele 3 metode

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pascu_Bianca_Lab1.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Bine ati venit!");
+        }
+        public IActionResult Salut()
+        {
+            return Content("Salut!");
+        }
+        public IActionResult Mesaj(string nume, int nr)
+        {
+            return Content(nume+" a selectat "+nr+" produse.");
+        }
+    }
+}
